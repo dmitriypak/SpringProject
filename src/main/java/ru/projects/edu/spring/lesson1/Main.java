a4159f8d7b673e69f7db220cfb8906e06be56cc8
Dmitriy Pak <fly2sky7777@mail.ru>
2019-06-02 12:54:02 +0300

    initial commit

---
+++ src/main/java/ru/projects/edu/spring/lesson1/Main.java
@@ -0,0 +1,11 @@
+package ru.projects.edu.spring.lesson1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+
+  }
+}
